5079e7fad3a9fa5ada395c3d43d34e6d64e69ab8
James Sinclair <sinclair.j@gmail.com>
2019-12-01 23:37:21 +1100

    bug fix for serving vue as index

--- backend/project4/urls.py
+++ backend/project4/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.static import static
+#from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('', include('register_machine.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
