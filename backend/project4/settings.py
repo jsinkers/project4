9869745d5ff127da9f4a7634d83a78c8468bad6a
James Sinclair <sinclair.j@gmail.com>
2019-12-01 20:06:03 +1100

    Updated settings and views to integrate Vue

--- backend/project4/settings.py
+++ backend/project4/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'project4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'reg-mac-frontend', 'dist')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -122,6 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'reg-mac-frontend', 'static')
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
--- backend/register_machine/views.py
+++ backend/register_machine/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.generic import TemplateView
+from django.views.decorators.cache import never_cache
+from rest_framework import viewsets
 
+
+# Serve Vue Application
+index_view = never_cache(TemplateView.as_view(template_name='index.html'))
 
 def index(request):
     data = {
