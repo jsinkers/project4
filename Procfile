40dd7522aa99a73d03e1111a361e857b037539a0
James Sinclair <sinclair.j@gmail.com>
2019-12-08 18:24:16 +1100

    Added new challenges. made sure it updates challenges in database on each build.

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 run: python backend/manage.py collect
 release: python backend/manage.py migrate
+release: python backend/manage.py runscript import_challenges
 web: gunicorn --chdir backend/ project4.wsgi --log-file -
--- reg-mac-frontend/src/main.js
+++ reg-mac-frontend/src/main.js
@@ -2,7 +2,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'bootstrap'
-//import '@mdi/font/css/materialdesignicons.css'
 import 'vue-material-design-icons/styles.css'
 
 import Vue from 'vue'
@@ -11,10 +10,6 @@ import router from './router'
 
 Vue.config.productionTip = false
 
-// import Vue from "vue";
-// import MenuIcon from "vue-material-design-icons"
-
-// Vue.component('menu-icon', MenuIcon);
 new Vue({
   router,
   el: '#app',
