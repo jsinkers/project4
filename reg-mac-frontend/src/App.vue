52d17bfc6c54f77dabed6bcc1e62571b0e985a77
James Sinclair <sinclair.j@gmail.com>
2022-12-08 15:10:20 +1100

    Update navbar text to Register Machine

--- reg-mac-frontend/src/App.vue
+++ reg-mac-frontend/src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col p-0">
                 <nav class="navbar navbar-expand-sm navbar-light bg-light">
-                    <router-link class="navbar-brand mb-0 h1" to="/">Reggie</router-link>
+                    <router-link class="navbar-brand mb-0 h1" to="/">Register Machine</router-link>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
